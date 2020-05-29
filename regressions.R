8314c611be8ad0e8730c0e2bd2337ee79025f996
Joel Berman <jberman@Joels-MacBook-Pro.local>
2020-05-28 19:32:22 -0700

    Fixed table layout

--- regressions.R
+++ regressions.R
@@ -4,11 +4,15 @@ library(ggplot2)
 library(ggpubr)
 library(gridExtra)
 library(zoo)
+library(stargazer)
+
+
+
 
 #########################################################################################################################
 #   What I think I did was in a note from Cynthia.  I have no confidence whether I did this correctly or not                               #
 #########################################################################################################################
-# Start on the day of 100 confirmed, drop countries with less than 100 confirmed or less than 10 dead    
+# Start on the day of 100 confirmed, drop countries with less than 100 confirmed or less than 10 dead
 # Regressions should go through origin
 #                                                                                                                       #
 # calculate log of difference: log(7dMA at time T - 7dMA at time t-1) vs. log of cumulative confirmed (x-axis)          #
@@ -24,22 +28,13 @@ library(zoo)
 ggplotRegression <- function (fit) {
   ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) +
     geom_point() +
-    stat_smooth(method = "lm", col = "red", formula = y ~ 0 + x) +       # force through origin
-    theme(plot.title = element_text( size = 10, face = "bold")) + 
+    stat_smooth(method = "lm",
+                col = "red",
+                formula = y ~ 0 + x) +       # force through origin
+    theme(plot.title = element_text(size = 10, face = "bold")) +
     labs(
-      title = paste("Intercept =", signif(fit$coef[[1]], 5),
-                    " P =", lmp(fit)),
-      subtitle = strsplit(as.character(fit$call[2]), split = "=")
+      title = strsplit(as.character(fit$call[2]), split = "=")
     )
-}
-#############################################################
-### Fornmula to create regression statistics for printing ###
-#############################################################
-lmp <- function (fit) {
-  f <- summary(fit)$fstatistic
-  p <- pf(f[1], f[2], f[3], lower.tail = F)
-  attributes(p) <- NULL
-  return(p)
 }
 
 
@@ -58,40 +53,52 @@ for (i in 1:length(c19ts)) {
   temp <- c19ts[[i]]          # pull the data frame to play with it
   names <- c19ts[i]           # Country name
   
-
-# Start from the 100th death AND 10th death
-    subset(temp, subset = (confirmed >= 100))
+  
+  # Start from the 100th death AND 10th death
+  temp <- subset(temp, subset = (confirmed >= 100))
   temp <-
     subset(temp, subset = (deaths >= 10))  # Drop rows til 10 deaths
+
   
-# Start from the 7th day because we use trailing 7 day moving average
+  # Start from the 7th day because we use trailing 7 day moving average
   
-  if (nrow(temp) > 6) {
-    temp$ma_confirmed <- rollmeanr(temp$confirmed, 7, na.pad = TRUE)  # 7 Day MA(moving average) of cases
-    temp$diff_ma_confirmed <- c(0, diff(temp$ma_confirmed))           # Diff of MA
-    temp$raw_diff_confirmed <- c(0, diff(temp$confirmed))             # Diff of daily cases
-    temp$total_cum_confirmed <- temp$confirmed                        # Total cases
-    temp$log_of_difference <-                                         # ln (1st difference of cases)
+  if (nrow(temp) > 7) {
+    temp$ma_confirmed <-
+      rollmeanr(temp$confirmed, 7, na.pad = TRUE)  # 7 Day MA(moving average) of cases
+    temp$diff_ma_confirmed <-
+      c(0, diff(temp$ma_confirmed))           # Diff of MA
+    temp$raw_diff_confirmed <-
+      c(0, diff(temp$confirmed))             # Diff of daily cases
+    temp$total_cum_confirmed <-
+      temp$confirmed                        # Total cases
+    temp$log_of_difference <-
+      # ln (1st difference of cases)
       log(temp$ma_confirmed - temp$diff_ma_confirmed)
-    temp$diff_of_log <-                                               # differences of logs                   
+    temp$diff_of_log <-
+      # differences of logs
       log(temp$ma_confirmed) - log(temp$diff_ma_confirmed)
-    temp$raw_diff <- temp$ma_confirmed - temp$diff_ma_confirmed       # raw difference of MA of cases
-    temp$log_cum_confirmed <- log(temp$total_cum_confirmed)           # ln(total cases)
+    temp$raw_diff <-
+      temp$ma_confirmed - temp$diff_ma_confirmed       # raw difference of MA of cases
+    temp$log_cum_confirmed <-
+      log(temp$total_cum_confirmed)           # ln(total cases)
   } else
     temp <-
     temp[FALSE]  # empty country if not at least 7 datapoints (should never be, because to have 100 confirmed in 7 days and no more is weird)
   
-  c19ts[[i]] <- temp                                                  # put computed data back into list of country data
+  c19ts[[i]] <-
+    temp                                                  # put computed data back into list of country data
 }
 
-c19ts <-                                                              # Drop all empty dataframes like magic
+c19ts <-
+  # Drop all empty dataframes like magic
   c19ts[sapply(c19ts, function(x)
     dim(x)[1]) > 0]
 ############################################################
 ### Start outout stuff - I separated this for simplicity ###
 ############################################################
 
-pdf(                                                                  # Output as Landscape PDF with narrow margins
+pdf(
+  # Output as Landscape PDF with narrow margins
   "plots.pdf",
   bg = "white",
   paper = "USr",
@@ -99,27 +106,65 @@ pdf(                                                                  # Output a
   height = 7.5
 )
 
-for (i in 1:(length(c19ts))) {                                        # Start loop through countries again
-  skip_to_next <- FALSE                                               # needed to catch math errors because
-  tryCatch({                                                          # some countries had decreasing total cases
-                                                                      # So this catches errors and skips that country
+for (i in 1:(length(c19ts))) {
+  # Start loop through countries again
+  skip_to_next <-
+    FALSE                                               # needed to catch math errors because
+  tryCatch({
+    # some countries had decreasing total cases
+    # So this catches errors and skips that country
     
-    temp <- na.omit(c19ts[[i]])                                       # Grab DF and COuntry name
+    temp <-
+      na.omit(c19ts[[i]])                                       # Grab DF and COuntry name
     Country <- names(c19ts[i])
     
-### Make R%egression PLot Objects - force X intercept to 0
-     p1 <-                                                             # Log of differences ~ log of cases
-      ggplotRegression(lm(log_of_difference ~   0 + log_cum_confirmed, data = temp))
-    p2 <-                                                             # Diff of logs ~  log of cases
-      ggplotRegression(lm(diff_of_log ~ 0 + log_cum_confirmed, data = temp))
-    p3 <-                                                             # Raw diff ~ cases
-      ggplotRegression(lm(raw_diff_confirmed ~ 0 + total_cum_confirmed, data = temp)) 
+    ### Make R%egression PLot Objects - force X intercept to 0
     
-    print(annotate_figure(                                            # Push page to PDF with Country Name
-      ggarrange(p1, p2, p3, ncol = 2, nrow = 2),
+    p1.fit <-
+      lm(log_of_difference ~   0 + log_cum_confirmed, data = temp)  # Log of differences ~ log of cases
+    p2.fit <-
+      lm(diff_of_log ~ 0 + log_cum_confirmed, data = temp)          # Diff of logs ~  log of cases
+    p3.fit <-
+      lm(raw_diff_confirmed ~ 0 + total_cum_confirmed, data = temp) # Raw diff ~ cases
+    
+    p1 <- ggplotRegression(p1.fit)
+    p2 <- ggplotRegression(p2.fit)
+    p3 <- ggplotRegression(p3.fit)
+    
+    text <-
+      toString(paste0(
+        stargazer(
+          p1.fit,
+          p2.fit,
+          p3.fit,
+          style = "all",
+          font.size = "normalsize",
+          type = "text",
+          omit.stat = c("adj.rsq","n"),omit.table.layout = "n"
+        ),
+        "\n"
+      ))
+    
+    p4 <- ggplot() +
+      annotation_custom(
+        text_grob(text, family = "Courier", size = 8),
+        xmin = -Inf,
+        xmax = Inf,
+        ymin = -Inf,
+        ymax = Inf
+      ) +
+      theme_void()
+    
+
+    
+    print(annotate_figure(
+      # Push page to PDF with Country Name
+      ggarrange(p4,ggarrange(p1,p2,p3,ncol=3),nrow=2)
+      ,
       top = text_grob(Country, color = "blue", size = 14)
     ))
-  }, error = function(e) {                                            # Error routine - Set skip_to_next = TRUE
+  }, error = function(e) {
+    # Error routine - Set skip_to_next = TRUE
     skip_to_next <- TRUE
   })
   if (skip_to_next) {
@@ -127,3 +172,5 @@ for (i in 1:(length(c19ts))) {                                        # Start lo
   }
 }
 dev.off()                                                             # Close PDF
+
+
